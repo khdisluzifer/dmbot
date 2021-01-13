a2d7c5713f5953cf577b38128fb22699da05fa8e
khdisluzifer <44751400+khdisluzifer@users.noreply.github.com>
2021-01-13 21:52:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ const twitterApp = {
   consumer_secret: process.env.CONSUMER_SECRET,
   access_token: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-  timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.
+  timeout_ms: 60 * 100 // optional HTTP request timeout to apply to all requests.
 };
 
 module.exports = {
   twitterApp,
-  userName: process.env.meetupyuk
+  userName: process.env.USERNAME
 };
