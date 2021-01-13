c94a6f86d034dc463f7042b0400a806e44f31b47
khdisluzifer <44751400+khdisluzifer@users.noreply.github.com>
2021-01-13 21:25:55 +0700

    Update AutoDM.js

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -41,7 +41,7 @@ const GenerateMessage = name => {
   ];
   const d = new Date();
   const dayName = days[d.getDay()];
-  return `Hi ${name} Thanks for .... \n Happy ${dayName} 😊😊 `; // your message
+  return `Hallo  ${name} Terimakasih Sudah follow meetupyuk.untuk menfess di base ini bisa send via Dm dengan tambahan kata m.y yaa Terimakasih \n Happy ${dayName} 😊😊 `; // your message
   // My message   return `Hi ${name} Thanks for being a part of my social media network. I'am the @PicsrushE founder,A new Online Image Editor completely with web technologies,I'm also a reactjs developer and medium blogger.\n Happy to discuss anytime 😊  \n Happy ${dayName} 😊😊 `;
 };
 
